335e36f00b450ffb1bda9620e6f0ed3e51b53f2e
Nikita Polyakov <nikitapolyakov001@gmail.com>
2021-04-07 22:44:53 +0300

    create tests

---
+++ lessons/32-testing/tests/browser.spec.js
@@ -0,0 +1,7 @@
+import { createFormHtml } from "../src/browser.js";
+
+// describe("render", () => {
+//   describe("createFormHtml", () => {
+//     it("should add correct elements", () => {});
+//   });
+// });
--- lessons/32-testing/tests/functions.spec.js
+++ lessons/32-testing/tests/functions.spec.js
@@ -1,12 +1,44 @@
-import { isValid, sendData, FormHandler } from '../src/functions'
+import { isValid, sendData, FormHandler } from "../src/functions";
 
-describe('Tests', () => {
-  it('', () => {
-    expect(true).toBeTruthy();
-  })
+describe("Tests", () => {
   // units
-  describe('isValid', () => {})
-  describe('sendData', () => {})
+  describe("isValid", () => {
+    it("Should be valid", () => {
+      expect(isValid("Alex")).toBeTruthy();
+    });
+    it("Should be valid with undefined", () => {
+      expect(isValid()).toBeFalsy();
+    });
+    it("Should be invalid with empty", () => {
+      expect(isValid("  ")).toBeFalsy();
+    });
+  });
+  describe("sendData", () => {
+    it("Should send data", () => {
+      const saveFn = jest.fn();
+      sendData("alex", saveFn);
+      expect(saveFn).toHaveBeenCalledWith("alex");
+    });
+  });
   // integration
-  describe('FormHandler', () => {})
-})
+  describe("FormHandler", () => {
+    it("If name is valid should send data", () => {
+      const isValidFn = jest.fn().mockImplementation(() => true);
+      const sendDataFn = jest.fn();
+      const formHandler = new FormHandler(isValidFn, sendDataFn);
+      const name = "nick";
+      formHandler.onSubmit(name);
+      expect(isValidFn).toHaveBeenCalledWith(name);
+      expect(sendDataFn).toHaveBeenCalledWith(name);
+    });
+    it("If name is invalid shouldn't send data", () => {
+      const isValidFn = jest.fn().mockImplementation(() => false);
+      const sendDataFn = jest.fn();
+      const formHandler = new FormHandler(isValidFn, sendDataFn);
+      const name = "nick";
+      formHandler.onSubmit(name);
+      expect(isValidFn).toHaveBeenCalledWith(name);
+      expect(sendDataFn).not.toHaveBeenCalled();
+    });
+  });
+});
