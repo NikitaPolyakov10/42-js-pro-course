9e6c1b5285ca5fd641322e77d1d9724bdcddd836
Nikita Polyakov <nikitapolyakov001@gmail.com>
2021-04-11 18:00:24 +0300

    lesson/33-data-structure

--- lessons/33-data-structure/index.js
+++ lessons/33-data-structure/index.js
@@ -1,21 +1,102 @@
-const quickSort = array => {
-  return array
-}
+const quickSort = (array) => {
+  const newArray = [...array];
+  if (newArray.length <= 1) {
+    return newArray;
+  }
 
-const toFlat = array => {
-  return array
-}
+  let index = Math.floor(newArray.length / 2);
+  let element = newArray[index];
+  let less = [];
+  let greater = [];
+
+  for (let i = 0; i < newArray.length; i++) {
+    if (i === index) {
+      continue;
+    }
+
+    if (newArray[i].order < element.order) {
+      less.push(newArray[i]);
+    } else {
+      greater.push(newArray[i]);
+    }
+  }
+
+  return [...quickSort(less), element, ...quickSort(greater)];
+};
+
+const toFlat = (array) => {
+  let queue = [...array];
+  let result = [];
+  while (queue.length) {
+    let curr = queue.pop();
+    if (Array.isArray(curr)) {
+      queue.push(...curr);
+    } else {
+      result.push(curr);
+    }
+  }
+  return result.reverse();
+};
 
 const binarySearch = (array, searchValue) => {
-  return 0
-}
+  let start = 0;
+  let end = array.length;
+  let mid;
+  let found = false;
+
+  while (found === false && start <= end) {
+    mid = Math.floor((start + end) / 2);
+
+    if (array[mid].amount === searchValue) {
+      found = true;
+      return array[mid];
+    }
+
+    if (searchValue < array[mid].amount) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return array[mid];
+};
 
 class HashTable {
-  constructor () {
-    this.map = {}
+  static getKeyByName(name) {
+    return name.charCodeAt(0);
   }
-  add (value) {}
-  find (name) {}
+  constructor() {
+    this.map = new Map();
+  }
+  add(value) {
+    const key = HashTable.getKeyByName(value.name);
+    if (!this.map.has(key)) {
+      this.map.set(key, new Map());
+    }
+    this.map.get(key).set(key, value);
+    return key;
+  }
+  find(name) {
+    const key = HashTable.getKeyByName(name);
+    const employeesMap = this.map.get(key);
+    if (!employeesMap) {
+      return;
+    }
+
+    return Array.from(employeesMap.values()).find((item) => item.name === name);
+  }
+  removeByName(name) {
+    const key = HashTable.getKeyByName(name);
+    let employeesMap = this.map.get(key);
+    if (!employeesMap) {
+      return;
+    }
+    return this.map.set(
+      key,
+      Array.from(employeesMap).filter((item) => item.name !== name)
+    );
+  }
 }
 
 module.exports = {
@@ -23,4 +104,4 @@ module.exports = {
   toFlat,
   binarySearch,
   HashTable,
-}
+};
