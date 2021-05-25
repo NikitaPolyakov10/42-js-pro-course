5cde8a278965c90c4bdff8b8bf17a8132b038ab7
Nikita Polyakov <nikitapolyakov001@gmail.com>
2021-05-25 20:19:48 +0300

    lesson 40 hw

---
+++ lessons/40-react-base/counter/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Counter from "./Components/Counter.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter value={0} />
+      <Counter value={0} />
+      <Counter value={0} />
+      <Counter value={0} />
+      <Counter value={0} />
+    </div>
+  );
+}
+
+export default App;
